547cc7ab80e8e14e4e8478d149a603e730dd18fb
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-19 19:41:07 -0300

    adicionando o currentTime e o Start

--- Program.cs
+++ Program.cs
@@ -5,7 +5,18 @@ namespace StopWatch
     {
         static void Main (string[] args)
         {
+            Start();
+        }
 
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+            while( currentTime <= time)
+            {
+                Console.WriteLine(currentTime);
+                currentTime++;
+            }
         }
     }    
 }
