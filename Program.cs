f4083908e85eaee96452793c8b0b8cb847e03b2c
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-19 20:28:16 -0300

    separando a opção time do menu

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace StopWatch
 
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0,data.Length-1));
         }
         static void Start(int time)
         {
