c9bc4fcc9810181790f287f753331af78a83c184
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-19 20:55:43 -0300

    .

--- obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
+++ obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82af1fb61c8eda3999741919c1a681a24a11246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a0a4489559096eaf7f619d2fb1d8ab8f4bb74b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
